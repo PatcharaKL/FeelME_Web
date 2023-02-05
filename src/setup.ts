5358ca358ff901d2476c1181edf91a4e03e9d6e8
Patchara <patchara.pck@gmail.com>
2023-02-06 03:32:57 +0700

    config vitest to refer setupFiles

--- src/setup.ts
+++ src/setup.ts
@@ -1,6 +1,5 @@
 import { afterAll, afterEach, beforeAll } from 'vitest'
 import { server } from './mocks/server'
-
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
