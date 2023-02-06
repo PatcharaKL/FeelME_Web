fef33a939e7b0620b9d46e9ed54a080c2ff88951
Patchara <patchara.pck@gmail.com>
2023-02-07 03:23:11 +0700

    extract health-check into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import "./App.css";
+import { HealthCheck } from "./components/HealthCheck";
 import { useGetHealthCheckQuery } from "./services/feelme_api";
 
 function App() {
@@ -6,19 +8,9 @@ function App() {
     useGetHealthCheckQuery({});
 
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <p>
-        Status: {!isLoading && !isError ? data?.message : JSON.stringify(error)}
-      </p>
-      <p>
-        {String(isError)}
-        {isLoading}
-      </p>
-      <p>
-        {import.meta.env.VITE_BASE_URL}/{endpointName}
-      </p>
-    </div>
+    <React.Fragment>
+      <HealthCheck />
+    </React.Fragment>
   );
 }
 
--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,9 +1,10 @@
 import { renderWithProviders } from "./utils/test-utils";
 import App from "./App";
-import { screen, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
 //* use 'screen.debug()' when want to output dom in terminal
 describe("test msw", () => {
+  //! fix app.tsx test
   it("should return healthy when call health-check endpoint", async () => {
     // Arrange
     renderWithProviders(<App />);
