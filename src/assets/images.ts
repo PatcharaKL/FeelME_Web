75bed7bbe44f3293e2454da55faa1fc11363a52f
Patchara <patchara.pck@gmail.com>
2023-02-08 17:07:03 +0700

    styled logo

---
+++ src/assets/images.ts
@@ -0,0 +1,3 @@
+export const images = {
+    "smile-icon": import('./smile.png'),
+}
--- src/components/FeelMeLogo.tsx
+++ src/components/FeelMeLogo.tsx
@@ -1,7 +1,14 @@
+import smileLogo from "../../src/assets/smile.png";
+
 export const FeelMeLogo = () => {
   return (
-    <div className="select-none text-4xl font-extrabold text-gray-700">
-      Feel<span className="select-none text-violet-700">Me</span>
-    </div>
+    <>
+      <div className="flex justify-center">
+        <img className="h-16 object-scale-down" src={smileLogo}></img>
+      </div>
+      <div className="select-none text-4xl font-semibold text-gray-700">
+        Feel<span className="font-extrabold text-violet-700">Them</span>
+      </div>
+    </>
   );
 };
