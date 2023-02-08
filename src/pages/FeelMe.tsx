78c2d26fc572c37f370c550cc06eb91649c29c80
Patchara <patchara.pck@gmail.com>
2023-02-09 00:27:27 +0700

    fix border radius and padding

--- src/pages/FeelMe.tsx
+++ src/pages/FeelMe.tsx
@@ -2,12 +2,12 @@ import SideBar from "../components/side-bar/SideBar";
 const FeelMe = () => {
   return (
     <>
-      <div className="h-screen w-screen bg-gradient-to-br from-white to-violet-100 p-6">
+      <div className="h-screen w-screen bg-gradient-to-br from-white to-violet-100 p-4">
         <div className="flex h-full gap-6">
           <SideBar />
           <div
             id="board"
-            className="flex-1 overflow-hidden rounded-xl bg-white drop-shadow-sm"
+            className="flex-1 overflow-hidden rounded-md bg-white drop-shadow-sm"
           ></div>
         </div>
       </div>
