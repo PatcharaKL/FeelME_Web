6ed862472277734d145310bffa89985d3acb2e1f
Patchara <patchara.pck@gmail.com>
2023-02-11 20:55:11 +0700

    add mock data file

---
+++ src/mocks/mock-data.ts
@@ -0,0 +1,22 @@
+export const happiness_points = {
+    id: 1,
+    period: "weekly",
+    record: [
+        {
+            happiness_points: {
+                self_points: 2,
+                work_points: 4,
+                co_worker_points: 8,
+            },
+            date: "2023-01-08",
+        },
+        {
+            happiness_points: {
+                self_points: -1,
+                work_points: 2,
+                co_worker_points: 1,
+            },
+            date: "2023-01-01",
+        },
+    ],
+}
