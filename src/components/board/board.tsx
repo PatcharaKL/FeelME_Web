53662b4d67518597348e22f8b9e356eded049b80
Patchara <patchara.pck@gmail.com>
2023-02-11 09:41:24 +0700

    extract board into seperate component

---
+++ src/components/board/board.tsx
@@ -0,0 +1,10 @@
+export const Board = () => {
+  return (
+    <div
+      id="board"
+      className="flex flex-1 animate-pulse flex-col justify-center overflow-hidden rounded-md bg-gray-500 text-center font-mono text-6xl font-black text-white drop-shadow-lg"
+    >
+      <div>LOADING...</div>
+    </div>
+  );
+};
