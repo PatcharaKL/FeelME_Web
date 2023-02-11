020217840c69f88c11652b7a7e10738db5128d5e
Patchara <patchara.pck@gmail.com>
2023-02-11 20:27:59 +0700

    mock get points handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,5 @@
 // src/mocks/handlers.js
+import { delay } from "@reduxjs/toolkit/dist/utils";
 import { rest } from "msw";
 
 const testUrl = (baseUrl: string) => {
@@ -17,29 +18,28 @@ export const handlers = [
   rest.get(testUrl("/users/1/happiness-points"), (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          self_points: 5,
-          work_points: 4,
-          co_worker_points: 3,
-          date: "2022-01-01",
-        },
-        {
-          id: 1,
-          self_points: 2,
-          work_points: -5,
-          co_worker_points: 3,
-          date: "2022-01-02",
-        },
-        {
-          id: 1,
-          self_points: 5,
-          work_points: 4,
-          co_worker_points: -3,
-          date: "2022-01-03",
-        },
-      ])
+      ctx.json({
+        id: 1,
+        period: "weekly",
+        record: [
+          {
+            happiness_points: {
+              self_points: 2,
+              work_points: 4,
+              co_worker_points: 8,
+            },
+            date: "2023-01-08",
+          },
+          {
+            happiness_points: {
+              self_points: -1,
+              work_points: 2,
+              co_worker_points: 1,
+            },
+            date: "2023-01-01",
+          },
+        ],
+      })
     );
   }),
 ];
