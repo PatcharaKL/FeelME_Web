f07a387d9c3d280a88950a2eca87867879b2606e
Patchara <patchara.pck@gmail.com>
2023-02-13 20:08:26 +0700

    checkpoint on employees

--- src/components/employees/Employees.tsx
+++ src/components/employees/Employees.tsx
@@ -1,9 +1,7 @@
 export const Employees = () => {
   return (
     <>
-      <div className="h-full w-full justify-center text-center align-middle text-3xl">
-        Hello Employees
-      </div>
+      <div className="text-lg">Hello Employees</div>
     </>
   );
 };
