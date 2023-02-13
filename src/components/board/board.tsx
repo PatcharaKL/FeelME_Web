c93b5059e5934e52e6c46bf278e8f577d3765069
Patchara <patchara.pck@gmail.com>
2023-02-13 13:45:46 +0700

    Selecteable sidebar

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,13 +1,28 @@
+import { useCallback } from "react";
+import { useAppSelector } from "../../app/hooks";
 import { Chart } from "../graph/BarChart";
 
 export const Board = () => {
+  const selectedID = useAppSelector(
+    (state) => state.sidebarSelection.selectedItemID
+  );
+
+  const Display = useCallback(() => {
+    if (selectedID === 1) {
+      return <Chart />;
+    }
+    if (selectedID === 2) {
+      return <div>Coming soon..</div>;
+    }
+    return <div>None..</div>;
+  }, [selectedID]);
+
   return (
     <div
       id="board"
       className="flex flex-1 items-center justify-center overflow-hidden rounded-md bg-white text-white drop-shadow-md"
     >
-      <Chart />
-      <Chart />
+      {Display()}
     </div>
   );
 };
