1aa8964d3f4e457b33d8e1930f1025ae6d41f7b7
Patchara <patchara.pck@gmail.com>
2023-02-23 23:32:34 +0700

    add authSlice

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store'
+
+// Define a type for the slice state
+interface AuthState {
+    authenticated: boolean;
+    accessToken: string;
+    refreshToken: string;
+}
+
+// Define the initial state using that type
+const initialState: AuthState = {
+    authenticated: false,
+    accessToken: "",
+    refreshToken: "",
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    // `createSlice` will infer the state type from the `initialState` argument
+    initialState,
+    reducers: {
+        login: (state, action: PayloadAction<AuthState>) => {
+            state.authenticated = true;
+            state.accessToken = action.payload.accessToken;
+            state.refreshToken = action.payload.refreshToken;
+        },
+        logout: (state) => {
+            state.authenticated = false;
+            state.accessToken = "";
+            state.refreshToken = "";
+        },
+    },
+})
+
+export const { login, logout } = authSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectSidebarSelectedItemID = (state: RootState) => state.sidebarSelection.selectedItemID
+
+export default authSlice.reducer
